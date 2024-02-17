10eeb9ea2ed76079a31248551d1b17751cd69a2f
pravin006 <pravin201197@gmail.com>
2024-02-17 17:54:14 +0800

    frontend user register & backend profile update

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS,USER_LOGIN_FAIL,USER_LOGOUT } from '../constants/UserConstants'
+import { USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS,USER_LOGIN_FAIL,USER_LOGOUT,
+USER_REGISTER_REQUEST,USER_REGISTER_SUCCESS,USER_REGISTER_FAIL } from '../constants/UserConstants'
 
 export const login = (email,password) => async (dispatch) =>{
     try{
@@ -31,4 +32,32 @@ export const login = (email,password) => async (dispatch) =>{
 export const logout = () => (dispatch) =>{
     localStorage.removeItem('userInfo')
     dispatch({type:USER_LOGOUT})
+}
+
+export const register = (name, email,password) => async (dispatch) =>{
+    try{
+        dispatch({type:USER_REGISTER_REQUEST})
+
+        const config = {headers:{
+            'Content-type':'application/JSON'
+        }}
+
+        const {data}= await axios.post(
+            '/api/users/register/',
+            {'name':name,'email':email, 'password': password},
+            config
+            )
+
+        dispatch({type:USER_REGISTER_SUCCESS, payload:data})
+        dispatch({type:USER_LOGIN_SUCCESS, payload:data})
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch(error){
+        dispatch({type:USER_REGISTER_FAIL, 
+            payload:error.response && error.response.data.detail
+                ? error.response.data.detail 
+                : error.message
+        })
+    }
 }
