9e02152fad4884f22f4d524c1f7681749e9be91b
pravin006 <pravin201197@gmail.com>
2024-02-25 18:57:49 +0800

    backend get all user orders & frontend display all orders

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { ORDER_CREATE_REQUEST,ORDER_CREATE_SUCCESS,ORDER_CREATE_FAIL,
 ORDER_DETAILS_REQUEST,ORDER_DETAILS_SUCCESS,ORDER_DETAILS_FAIL,
-ORDER_PAY_REQUEST,ORDER_PAY_SUCCESS,ORDER_PAY_FAIL,ORDER_PAY_RESET } from '../constants/orderConstants'
+ORDER_PAY_REQUEST,ORDER_PAY_SUCCESS,ORDER_PAY_FAIL,ORDER_PAY_RESET,
+ORDER_LIST_MY_REQUEST,ORDER_LIST_MY_SUCCESS,ORDER_LIST_MY_FAIL,ORDER_LIST_MY_RESET  } from '../constants/orderConstants'
 import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 
 export const createOrder = (order) => async (dispatch, getState) =>{
@@ -83,6 +84,33 @@ export const payOrder = (id,paymentResult) => async (dispatch, getState) =>{
 
     } catch(error){
         dispatch({type:ORDER_PAY_FAIL, 
+            payload:error.response && error.response.data.detail
+                ? error.response.data.detail 
+                : error.message
+        })
+    }
+}
+
+export const listMyOrders = () => async (dispatch, getState) =>{
+    try{
+        dispatch({type:ORDER_LIST_MY_REQUEST})
+
+        const {userLogin: {userInfo},} = getState()
+
+        const config = {headers:{
+            'Content-type':'application/JSON',
+            Authorization: `Bearer ${userInfo.token}`
+        }}
+
+        const {data}= await axios.get(
+            `/api/orders/myorders/`,
+            config
+            )
+
+        dispatch({type:ORDER_LIST_MY_SUCCESS, payload:data})
+
+    } catch(error){
+        dispatch({type:ORDER_LIST_MY_FAIL, 
             payload:error.response && error.response.data.detail
                 ? error.response.data.detail 
                 : error.message
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import { USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS,USER_LOGIN_FAIL,USER_LOGOUT,
 USER_REGISTER_REQUEST,USER_REGISTER_SUCCESS,USER_REGISTER_FAIL,
 USER_DETAILS_REQUEST,USER_DETAILS_SUCCESS,USER_DETAILS_FAIL,USER_DETAILS_RESET,
 USER_UPDATE_PROFILE_REQUEST,USER_UPDATE_PROFILE_SUCCESS,USER_UPDATE_PROFILE_FAIL } from '../constants/UserConstants'
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 export const login = (email,password) => async (dispatch) =>{
     try{
@@ -35,6 +36,7 @@ export const logout = () => (dispatch) =>{
     localStorage.removeItem('userInfo')
     dispatch({type:USER_LOGOUT})
     dispatch({type:USER_DETAILS_RESET})
+    dispatch({type:ORDER_LIST_MY_RESET})
 }
 
 export const register = (name, email,password) => async (dispatch) =>{
