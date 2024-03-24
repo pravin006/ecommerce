a960bc3d3a6d63683a27c1122e14d3779b86e051
pravin006 <pravin201197@gmail.com>
2024-03-24 20:45:35 +0800

    paginator backend & frontend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,15 +9,15 @@ import { listProducts } from '../actions/productActions'
 
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-
+import Paginate from '../components/Paginate'
 
 function HomeScreen() {
   const dispatch = useDispatch()
   const productList = useSelector(state => state.productList)
-  const {error, loading, products} = productList
+  const {error, loading, products, page, pages} = productList
 
   const location = useLocation()
-  let keyword = location.search
+  const keyword = location.search
 
   useEffect(() =>{
     dispatch(listProducts(keyword))    
@@ -31,13 +31,16 @@ function HomeScreen() {
         {loading ? <Loader/>
           : error? <Message variant='danger'>{error}</Message>
           :
-          <Row>
-            {products.map(product =>(
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product}/>
-                </Col>
-            ))}
-          </Row>
+          <div>
+            <Row>
+              {products.map(product =>(
+                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                      <Product product={product}/>
+                  </Col>
+              ))}
+            </Row>
+            <Paginate page = {page} pages = {pages} keyword = {keyword}/>
+          </div>
           }
         
     </div>
