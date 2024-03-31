ebc06d5171cfd6526d858b563695f0770f6152b1
pravin006 <pravin201197@gmail.com>
2024-03-31 16:53:35 +0800

    migrate to postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,10 +131,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'pravin20',
+        'HOST':'localhost',
+        'POST': '5432'
     }
 }
 
